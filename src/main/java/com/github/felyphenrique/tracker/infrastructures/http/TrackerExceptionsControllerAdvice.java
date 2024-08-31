83a273bc6946d646e7c628155a012670bdcae51f
Felyp-Henrique <felyphenrique2015@gmail.com>
2024-08-31 16:39:00 -0300

    dev: feito todo o mapeamento das principais exceptions do projeto

--- src/main/java/com/github/felyphenrique/tracker/infrastructures/http/TrackerExceptionsControllerAdvice.java
+++ src/main/java/com/github/felyphenrique/tracker/infrastructures/http/TrackerExceptionsControllerAdvice.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.felyphenrique.tracker.kernel.exceptions.TrackerException;
 import com.github.felyphenrique.tracker.kernel.exceptions.TrackerNotFoundException;
+import com.github.felyphenrique.tracker.kernel.exceptions.TrackerSecurityException;
+import com.github.felyphenrique.tracker.kernel.exceptions.TrackerUnregisterException;
 
 @ControllerAdvice()
 public class TrackerExceptionsControllerAdvice {
@@ -18,6 +21,41 @@ public class TrackerExceptionsControllerAdvice {
         final ErrorResponse response = ErrorResponse
                 .builder(exception, HttpStatus.NOT_FOUND, exception.getMessage())
                 .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
+    }
+
+    @ExceptionHandler({ TrackerSecurityException.class })
+    public @ResponseBody() ResponseEntity<ErrorResponse> handleTrackerSecurityException(
+            TrackerSecurityException exception) {
+        final ErrorResponse response = ErrorResponse
+                .builder(exception, HttpStatus.UNAUTHORIZED, exception.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(response);
+    }
+
+    @ExceptionHandler({ TrackerUnregisterException.class })
+    public @ResponseBody() ResponseEntity<ErrorResponse> handlerTrackerUnregisterException(
+            TrackerUnregisterException exception) {
+        final ErrorResponse response = ErrorResponse
+                .builder(exception, HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+
+    @ExceptionHandler({ TrackerException.class })
+    public @ResponseBody() ResponseEntity<ErrorResponse> handlerTrackerException(
+            TrackerException exception) {
+        final ErrorResponse response = ErrorResponse
+                .builder(exception, HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
     }
 }
